cb42581cd18f5be58534009a17c44e8487474e50
Margaro <sichibukay@icloud.com>
2017-09-26 17:33:23 -0500

    se cambio a commworld

--- Comunicacion/comunicacion2.c
+++ Comunicacion/comunicacion2.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[]){
   MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     send = rank+1;
-    MPI_Gather(&send, 1, MPI_INTEGER, mensaje, 1, MPI_INTEGER, 0, &rank);
+    MPI_Gather(&send, 1, MPI_INTEGER, mensaje, 1, MPI_INTEGER, 0, MPI_COMM_WORLD);
     if(rank == 0) {
       printf("Resultado: %d %d %d %d %d %d %d %d\n", mensaje[0], mensaje[1], mensaje[2], mensaje[3], mensaje[4], mensaje[5], mensaje[6], mensaje[7] );
     }
